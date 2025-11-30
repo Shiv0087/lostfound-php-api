08ba87d07a5e4f250c690af35f2ba64ae81b8a74
Shiv <jadejas.k7777@gmail.com>
2025-11-30 19:08:11 +0530

    Update database connection configuration

--- public/db_config.php
+++ public/db_config.php
@@ -1,13 +1,12 @@
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = ""; // Leave the password empty
-    $dbname = "lostandfound_db";
+<?php
+$host = "johnny.heliohost.org";  //
+$username = "lostandfound_user";
+$password = "j64@bp4h?VbYGsec";
+$database = "lostandfound_db";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $database, 3306);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    ?>
-    
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
