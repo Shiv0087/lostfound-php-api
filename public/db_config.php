005610c0d1c782740994a6389ac213acf2117a4c
Shiv <jadejas.k7777@gmail.com>
2025-11-30 22:20:50 +0530

    Update db_config.php

--- public/db_config.php
+++ public/db_config.php
@@ -1,10 +1,10 @@
-<?php
-$host = "johnny.heliohost.org";  //
+<?php 
+$host = "johnny-mysql.heliohost.org";  
 $username = "lostandfound_user";
 $password = "j64@bp4h?VbYGsec";
 $database = "lostandfound_db";
 
-$conn = new mysqli($host, $username, $password, $database, 3306);
+$conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
